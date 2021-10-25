c7e6fa6d4a662018b3925d60b7ee52a1a8e4114c
Rakeshpro9040 <57039842+Rakeshpro9040@users.noreply.github.com>
2021-10-25 09:47:45 +0530

    appsettings json file

--- DotNetCorePractice/EmployeeManagement/Startup.cs
+++ DotNetCorePractice/EmployeeManagement/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -10,8 +11,17 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement
 {
+
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        // Notice we are using Dependency Injection here
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -32,7 +42,7 @@ namespace EmployeeManagement
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                    await context.Response.WriteAsync(_configuration["MyKey"]);
                 });
             });
         }

adfcb60c5516190a4e982b02be10f96bd260a568
Rakeshpro9040 <57039842+Rakeshpro9040@users.noreply.github.com>
2021-10-27 09:05:42 +0530

    MVC Setup

--- DotNetCorePractice/EmployeeManagement/Startup.cs
+++ DotNetCorePractice/EmployeeManagement/Startup.cs
@@ -27,6 +27,7 @@ namespace EmployeeManagement
         // visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure
@@ -40,9 +41,11 @@ namespace EmployeeManagement
 
             app.UseStaticFiles();
 
+            app.UseMvcWithDefaultRoute();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hosting Environment: " + env.EnvironmentName);
+                await context.Response.WriteAsync("Hello from Configure()");
             });
         }
     }
@@ -50,4 +53,6 @@ namespace EmployeeManagement
 
 //Testing
 /*
+ * http://localhost:24843/ --> Hello from MVC
+ * http://localhost:24843/about/index --> Hello from Configure()
 */
